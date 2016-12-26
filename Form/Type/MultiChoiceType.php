39a50738825d7de16c6542dfc4d1c2259b0772f5
dentiman <den.timanovskiy@gmail.com>
2016-12-26 16:00:50 +0200

    add notify

--- Form/Type/MultiChoiceType.php
+++ Form/Type/MultiChoiceType.php
@@ -4,7 +4,7 @@
 namespace Dentiman\BootstrapBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 
@@ -21,7 +21,7 @@ class MultiChoiceType extends AbstractType
         }
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'attr' => array(
